c86f0fac45b05e3c6f73e431bd462dc242626b67
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-12-01 19:27:10 -0500

    Add some dummy docstrings

--- include/gen/trace.h
+++ include/gen/trace.h
@@ -7,8 +7,19 @@
 
 class Trace {
 public:
+    /**
+     * @return the choice trie.
+     */
     [[nodiscard]] virtual Trie get_choice_trie() const = 0;
+
+    /**
+     * @return the log joint density.
+     */
     [[nodiscard]] virtual double get_score() const = 0;
+
+    /**
+     * @return the return value of the generative function.
+     */
     [[nodiscard]] virtual std::any get_return_value() const = 0;
 };
 
