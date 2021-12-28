914abeeeec43a96987013e3e4017a4c0d7661350
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-12-27 20:14:36 -0500

    make conversions work for arbitrary vector and pair types

--- include/gen/conversions.h
+++ include/gen/conversions.h
@@ -9,30 +9,82 @@ using std::pair;
 
 // Data types that can be used as arguments and return values of generative functions
 
+// ***********************
+// ** unsupported types **
+// ***********************
+
+// TODO: consider emitting a warning
+
+template <typename T>
+void unroll(vector<Tensor>& unrolled, const T& value) {
+}
+
+template <typename T>
+pair<size_t,T> roll(const vector<Tensor>& unrolled, size_t start, const T& value) {
+    return {start, value};
+}
+
 // ************
 // ** Tensor **
 // ************
 
-vector<Tensor> unroll(const Tensor& args);
-Tensor roll(const vector<Tensor>& unrolled, const Tensor& value);
+pair<size_t,Tensor> roll(const vector<Tensor>& unrolled, size_t start, const Tensor& value);
+void unroll(vector<Tensor>& unrolled, const Tensor& value);
 
-// *************************
-// ** pair<Tensor,Tensor> **
-// *************************
+// ***************
+// ** pair<T,U> **
+// ***************
 
-vector<Tensor> unroll(const pair<Tensor, Tensor>& args);
-pair<Tensor, Tensor> roll(const vector<Tensor>& unrolled, const pair<Tensor, Tensor>& value);
+template <typename T, typename U>
+void unroll(vector<Tensor>& unrolled, const pair<T,U>& value) {
+    unroll(unrolled, value.first);
+    unroll(unrolled, value.second);
+}
 
-// **********************
-// ** pair<Tensor,int> **
-// **********************
+template <typename T, typename U>
+pair<size_t,pair<T,U>> roll(const vector<Tensor>& unrolled, size_t start, const pair<T,U>& value) {
+    auto [after_first, rolled_first] = roll(unrolled, start, value.first);
+    auto [after_second, rolled_second] = roll(unrolled, after_first, value.second);
+    return {after_second, {rolled_first, rolled_second}};
+}
 
-vector<Tensor> unroll(const pair<Tensor, int>& args);
-pair<Tensor, int> roll(const vector<Tensor>& unrolled, const pair<Tensor, int>& value);
+// ***************
+// ** vector<T> **
+// ***************
 
-// ********************
-// ** vector<Tensor> **
-// ********************
+template <typename T>
+void unroll(vector<Tensor>& unrolled, const vector<T>& value) {
+    for (const auto element: value) {
+        unroll(unrolled, element);
+    }
+}
 
-vector<Tensor> unroll(const vector<Tensor>& args);
-vector<Tensor> roll(const vector<Tensor>& unrolled, const vector<Tensor>& value);
+template <typename T>
+pair<size_t,vector<T>> roll(const vector<Tensor>& unrolled, size_t start, const vector<T>& value) {
+    vector<T> rolled;
+    for (const auto element: value) {
+        auto [new_start, rolled_element] = roll(unrolled, start, element);
+        start = new_start;
+        rolled.emplace_back(rolled_element);
+    }
+    return {start, rolled};
+}
+
+
+// *************
+// ** general **
+// *************
+
+template <typename T>
+vector<Tensor> unroll(const T& value) {
+    std::vector<Tensor> unrolled;
+    unroll(unrolled, value);
+    return unrolled;
+}
+
+template <typename T>
+T roll(const vector<Tensor>& unrolled, const T& value) {
+    auto [num_tensors, rolled] = roll(unrolled, 0, value);
+    return rolled;
+}
+
--- src/conversions.cpp
+++ src/conversions.cpp
@@ -12,51 +12,50 @@ using std::pair;
 // ** Tensor (scalar numerical value) **
 // *************************************
 
-vector<Tensor> unroll(const Tensor& args) {
-    return {args};
+pair<size_t,Tensor> roll(const vector<Tensor>& unrolled, size_t start, const Tensor& value) {
+    return {start + 1, unrolled[start]};
 }
 
-Tensor roll(const vector<Tensor>& unrolled, const Tensor& value) {
-    assert(unrolled.size() == 1);
-    return unrolled[0];
+void unroll(vector<Tensor>& unrolled, const Tensor& value) {
+    unrolled.emplace_back(value);
 }
+//
+//// *************************
+//// ** pair<Tensor,Tensor> **
+//// *************************
+//
+//vector<Tensor> unroll(const pair<Tensor, Tensor>& args) {
+//    vector<Tensor> tensors {args.first, args.second};
+//    return move(tensors);
+//}
+//
+//pair<Tensor, Tensor> roll(const vector<Tensor>& unrolled, const pair<Tensor, Tensor>& value) {
+//    assert(unrolled.size() == 2);
+//    return {unrolled[0], unrolled[1]};
+//}
+//
+//// **********************
+//// ** pair<Tensor,int> **
+//// **********************
+//
+//vector<Tensor> unroll(const pair<Tensor, int>& args) {
+//    vector<Tensor> tensors {args.first};
+//    return move(tensors);
+//}
+//
+//pair<Tensor, int> roll(const vector<Tensor>& unrolled, const pair<Tensor, int>& value) {
+//    assert(unrolled.size() == 1);
+//    return {unrolled[0], value.second};
+//}
 
-// *************************
-// ** pair<Tensor,Tensor> **
-// *************************
-
-vector<Tensor> unroll(const pair<Tensor, Tensor>& args) {
-    vector<Tensor> tensors {args.first, args.second};
-    return move(tensors);
-}
-
-pair<Tensor, Tensor> roll(const vector<Tensor>& unrolled, const pair<Tensor, Tensor>& value) {
-    assert(unrolled.size() == 2);
-    return {unrolled[0], unrolled[1]};
-}
-
-// **********************
-// ** pair<Tensor,int> **
-// **********************
-
-vector<Tensor> unroll(const pair<Tensor, int>& args) {
-    vector<Tensor> tensors {args.first};
-    return move(tensors);
-}
-
-pair<Tensor, int> roll(const vector<Tensor>& unrolled, const pair<Tensor, int>& value) {
-    assert(unrolled.size() == 1);
-    return {unrolled[0], value.second};
-}
-
-// ********************
-// ** vector<Tensor> **
-// ********************
-
-vector<Tensor> unroll(const vector<Tensor>& args) {
-    return args;
-}
-
-vector<Tensor> roll(const vector<Tensor>& unrolled, const vector<Tensor>& value) {
-    return unrolled;
-}
+//// ********************
+//// ** vector<Tensor> **
+//// ********************
+//
+//vector<Tensor> unroll(const vector<Tensor>& args) {
+//    return args;
+//}
+//
+//vector<Tensor> roll(const vector<Tensor>& unrolled, const vector<Tensor>& value) {
+//    return unrolled;
+//}
