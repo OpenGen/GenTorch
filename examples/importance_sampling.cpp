692049ad640657b7cd93b14e31761597fe9824ff
Marco Cusumano-Towner <imarcoam@gmail.com>
2022-01-28 20:23:03 -0500

    use seed sequences in the existing monte carlo examples

--- examples/importance_sampling.cpp
+++ examples/importance_sampling.cpp
@@ -4,6 +4,9 @@
 #include <gen/parameters.h>
 #include <gen/distributions/normal.h>
 #include <gen/distributions/uniform_continuous.h>
+#include <gen/utils/randutils.h>
+
+using randutils::seed_seq_fe128;
 
 using torch::Tensor, torch::tensor;
 using std::vector, std::cout, std::endl;
@@ -40,11 +43,10 @@ namespace gen::examples::importance_sampling {
         }
     };
 
-    void do_chunk(size_t thread_idx, std::vector<double>& result, size_t num_samples) {
+    void do_chunk(size_t thread_idx, std::vector<double>& result, seed_seq_fe128& seed_seq, size_t num_samples) {
         c10::InferenceMode guard{true}; // should be unnecessary, since no new Tensors are created
         EmptyModule parameters;
-        std::random_device rd{};
-        std::mt19937 rng{rd()};
+        std::mt19937 rng(seed_seq);
         auto model = Model();
         auto proposal = Proposal();
         double total = 0;
@@ -71,11 +73,15 @@ namespace gen::examples::importance_sampling {
         return total;
     }
 
-    double estimate(size_t num_threads, size_t num_samples_per_thread) {
+    double estimate(size_t num_threads, size_t num_samples_per_thread, uint32_t seed) {
+        seed_seq_fe128 parent_seq = {seed};
+        std::vector<seed_seq_fe128> child_seqs;
         std::vector<std::thread> threads {num_threads};
         std::vector<double> result(num_threads);
         for (size_t thread_idx = 0; thread_idx < num_threads; thread_idx++) {
-            threads[thread_idx] = std::thread(do_chunk, thread_idx, std::ref(result), num_samples_per_thread);
+            seed_seq_fe128& child_seq = child_seqs.emplace_back(parent_seq.spawn());
+            threads[thread_idx] = std::thread(do_chunk, thread_idx, std::ref(result),
+                                              std::ref(child_seq), num_samples_per_thread);
         }
         for (auto& thread: threads) {
             thread.join();
@@ -85,23 +91,27 @@ namespace gen::examples::importance_sampling {
 
 }
 
-const std::string usage = "Usage: ./importance_sampling <num_threads> <num_samples_per_thread>";
+const std::string usage = "Usage: ./importance_sampling <num_threads> <num_samples_per_thread> <seed>";
 
 int main(int argc, char* argv[]) {
     torch::set_num_interop_threads(1);
     torch::set_num_threads(1);
-    if (argc != 3) {
+    if (argc != 4) {
         throw std::invalid_argument(usage);
     }
     size_t num_threads;
     size_t num_samples_per_thread;
+    uint32_t seed;
     try {
         num_threads = std::atoi(argv[1]);
         num_samples_per_thread = std::atoi(argv[2]);
+        seed = std::atoi(argv[3]);
     } catch (const std::invalid_argument& e) {
         throw std::invalid_argument(usage);
     }
-    cout << "num_threads: " << num_threads << ", num_samples_per_thread: " << num_samples_per_thread << endl;
-    double result = gen::examples::importance_sampling::estimate(num_threads, num_samples_per_thread);
+    cout << "num_threads: " << num_threads << endl;
+    cout << "num_samples_per_thread: " << num_samples_per_thread << endl;
+    cout << "seed: " << seed << endl;
+    double result = gen::examples::importance_sampling::estimate(num_threads, num_samples_per_thread, seed);
     cout << "result: " << result << endl;
 }
